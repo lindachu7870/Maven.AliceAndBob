47cb350152cd88f534227344c9035b40b4b08b5c
Meilin Li (Linda) <limeilinda@gmail.com>
2022-06-23 22:31:43 -0400

    Added user input and greeting

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+//scanner
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
 public class Main {
 
     public static void main(String[] args ){
+        Scanner myName = new Scanner(System.in); //creates scanner object
+        System.out.println("Enter name");
 
+        String userName = myName.nextLine(); //reads user input
+        System.out.println("My name is " + userName); //outputs user input
+
+        if (userName.equals("Alice") || userName.equals("Bob")) {
+            System.out.println("Greetings " + userName + "!");
+        }
+        else {
+            System.out.println("");
+        }
     }
 }
